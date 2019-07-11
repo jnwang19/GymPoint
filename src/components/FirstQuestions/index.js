3d37f18da58f4ce847fee838cc582d5ecbb6dbfc
Jonathan Wang <jnwang19@gmail.com>
2019-07-11 22:50:53 +0800

    questions animation

--- src/components/FirstQuestions/index.js
+++ src/components/FirstQuestions/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styles from './FirstQuestions.module.css';
 
 import { withAuthorization } from '../Session';
 
 const FirstQuestionsPage = () => (
   <div>
-    <h1>First Questions</h1>
+    <FirstQuestions />
   </div>
 );
+
+class FirstQuestions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNext: false
+    };
+  };
+
+  nextQuestion = () => {
+    this.setState({isNext: true});
+  };
+
+  prevQuestion = () => {
+    this.setState({isNext: false});
+  };
+
+  render() {
+    return (
+      <div>
+        <div className={this.state.isNext ? [styles.question, styles.first, styles.firstclosed].join(' ')
+        : [styles.question, styles.first].join(' ')}>
+          <div className={[styles.button, styles.nextbutton].join(' ')} onClick={this.nextQuestion}></div>
+        </div>
+        <div className={this.state.isNext ? [styles.question, styles.second].join(' ')
+        : [styles.question, styles.second, styles.secondclosed].join(' ')}>
+        </div>
+        <div className={[styles.button, styles.prevbutton].join(' ')} onClick={this.prevQuestion}></div>
+      </div>
+    );
+  }
+}
 
 const condition = authUser => !!authUser;
 
